04a1e91a7026e6242a9c07b79e42991e51e47109
kidtzz <bangkitpr4@gmail.com>
2022-03-13 09:35:18 +0700

    newww

--- src/component/gallery/Gallery.js
+++ src/component/gallery/Gallery.js
@@ -54,16 +54,16 @@ function Gallery() {
                                         <h4>App 1</h4>
                                         <p>App</p>
                                         <div class="portfolio-links">
-                                            <SRLWrapper>
-                                                <a
-                                                    href={image.imageName}
-                                                    // data-gallery="portfolioGallery"
-                                                    class="portfolio-lightbox"
-                                                    title="App 1"
-                                                >
-                                                    <i class="bi bi-plus"></i>
-                                                </a>
-                                            </SRLWrapper>
+                                            {/* <SRLWrapper> */}
+                                            <a
+                                                href={image.imageName}
+                                                data-gallery="portfolioGallery"
+                                                class="portfolio-lightbox"
+                                                title="App 1"
+                                            >
+                                                <i class="bi bi-plus"></i>
+                                            </a>
+                                            {/* </SRLWrapper> */}
                                             <a
                                                 href=" "
                                                 class="portfolio-details-lightbox"
