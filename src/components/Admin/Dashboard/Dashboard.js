256ddcf1190b8e282c5bd9b33f3aa4b8a1a5e1b4
kidtzz <bangkitpr4@gmail.com>
2022-06-23 20:15:55 +0700

    update

--- src/components/Admin/Dashboard/Dashboard.js
+++ src/components/Admin/Dashboard/Dashboard.js
@@ -43,77 +43,81 @@ function Dashboard() {
                     Logout
                 </button>
             </div>
-            <div class="wrapper d-flex align-items-stretch">
+            <div className="wrapper d-flex align-items-stretch">
                 <nav id="sidebar">
-                    <div class="custom-menu">
+                    <div className="custom-menu">
                         <button
                             type="button"
                             id="sidebarCollapse"
-                            class="btn btn-primary"
+                            className="btn btn-primary"
                         >
-                            <i class="fa fa-bars"></i>
-                            <span class="sr-only">Toggle Menu</span>
+                            <i className="fa fa-bars"></i>
+                            <span className="sr-only">Toggle Menu</span>
                         </button>
                     </div>
-                    <div class="p-4">
+                    <div className="p-4">
                         <h1>
-                            <a href="index.html" class="logo">
+                            <a href="index.html" className="logo">
                                 Portfolic <span>Portfolio Agency</span>
                             </a>
                         </h1>
-                        <ul class="list-unstyled components mb-5">
-                            <li class="active">
+                        <ul className="list-unstyled components mb-5">
+                            <li className="active">
                                 <a href=" ">
-                                    <span class="fa fa-home mr-3"></span> Home
+                                    <span className="fa fa-home mr-3"></span>{" "}
+                                    Home
                                 </a>
                             </li>
                             <li>
                                 <a href=" ">
-                                    <span class="fa fa-user mr-3"></span> About
+                                    <span className="fa fa-user mr-3"></span>{" "}
+                                    About
                                 </a>
                             </li>
                             <li>
                                 <a href=" ">
-                                    <span class="fa fa-briefcase mr-3"></span>
+                                    <span className="fa fa-briefcase mr-3"></span>
                                     Works
                                 </a>
                             </li>
                             <li>
                                 <a href=" ">
-                                    <span class="fa fa-sticky-note mr-3"></span>
+                                    <span className="fa fa-sticky-note mr-3"></span>
                                     Blog
                                 </a>
                             </li>
                             <li>
                                 <a href=" ">
-                                    <span class="fa fa-suitcase mr-3"></span>
+                                    <span className="fa fa-suitcase mr-3"></span>
                                     Gallery
                                 </a>
                             </li>
                             <li>
                                 <a href=" ">
-                                    <span class="fa fa-cogs mr-3"></span>
+                                    <span className="fa fa-cogs mr-3"></span>
                                     Services
                                 </a>
                             </li>
                             <li>
                                 <a href=" ">
-                                    <span class="fa fa-paper-plane mr-3"></span>
+                                    <span className="fa fa-paper-plane mr-3"></span>
                                     Contacts
                                 </a>
                             </li>
                         </ul>
 
-                        <div class="mb-5">
-                            <h3 class="h6 mb-3">Subscribe for newsletter</h3>
-                            <form action=" " class="subscribe-form">
-                                <div class="form-group d-flex">
-                                    <div class="icon">
-                                        <span class="icon-paper-plane"></span>
+                        <div className="mb-5">
+                            <h3 className="h6 mb-3">
+                                Subscribe for newsletter
+                            </h3>
+                            <form action=" " className="subscribe-form">
+                                <div className="form-group d-flex">
+                                    <div className="icon">
+                                        <span className="icon-paper-plane"></span>
                                     </div>
                                     <input
                                         type="text"
-                                        class="form-control"
+                                        className="form-control"
                                         placeholder="Enter Email Address"
                                     />
                                 </div>
@@ -122,8 +126,8 @@ function Dashboard() {
                     </div>
                 </nav>
 
-                <div id="content" class="p-4 p-md-5 pt-5">
-                    <h2 class="mb-4">Sidebar #05</h2>
+                <div id="content" className="p-4 p-md-5 pt-5">
+                    <h2 className="mb-4">Sidebar #05</h2>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit, sed do eiusmod tempor incididunt ut labore et
