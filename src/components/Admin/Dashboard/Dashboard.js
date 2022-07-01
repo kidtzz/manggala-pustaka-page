3ac26feacbeb19d7a2a9535765c141c379f00b39
kidtzz <bangkitpr4@gmail.com>
2022-07-02 01:15:44 +0700

    update

--- src/components/Admin/Dashboard/Dashboard.js
+++ src/components/Admin/Dashboard/Dashboard.js
@@ -132,9 +132,7 @@ function Dashboard() {
                         nostrud exercitation ullamco laboris nisi ut aliquip ex
                         ea commodo consequat. Duis aute irure dolor in
                         reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
+                        fugiat nulla pariatur.
                     </p>
                 </div>
             </div>
